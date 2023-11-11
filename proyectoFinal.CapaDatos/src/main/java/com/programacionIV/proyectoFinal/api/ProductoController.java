ed517888495724d4be8b110d67ac69219db31807
Juan <jualmu07@gmail.com>
2023-11-11 00:18:36 -0600

    Documentacion de codigo

--- proyectoFinal.CapaDatos/src/main/java/com/programacionIV/proyectoFinal/api/ProductoController.java
+++ proyectoFinal.CapaDatos/src/main/java/com/programacionIV/proyectoFinal/api/ProductoController.java
@@ -21,35 +21,49 @@ import com.programacionIV.proyectoFinal.servicios.ProductoServicio;
 
 @RestController
 public class ProductoController {
-	
+
 	private final ProductoServicio productoServicio;
 
 	public ProductoController(ProductoServicio productoServicio) {
 		this.productoServicio = productoServicio;
 	}
-	
+
 	@GetMapping(path = "/productos", produces = "application/json")
-	public Page<Producto> listadoProductos(@PageableDefault(sort = "fechaInclusion", direction = Sort.Direction.DESC) Pageable pageable){
-		//Pageable pageable = PageRequest.of(0, 5,Sort.by(Sort.Direction.DESC,"vecesComprado"));
-		
+	public Page<Producto> listadoProductos(
+			@PageableDefault(sort = "fechaInclusion", direction = Sort.Direction.DESC) Pageable pageable) {
+
 		Page<Producto> consultaProductos = productoServicio.listaProductos(pageable);
-		
+
 		return consultaProductos;
-		
-		
+
 	}
-	
+
 	@GetMapping(path = "/productos/filtered", produces = "application/json")
-	public Page<Producto> listadoProductosFiltered(@RequestParam(name = "categorias", required = true) List<String> categorias, Pageable pageable){;
-		
-		
-		Page<Producto> consultaProductos = productoServicio.listaProductoFiltered(categorias,pageable);
-		
+	/**
+	 * Devuelve una lista de productos filtrada mediante el query param
+	 * "categorias", para utilizarlo se agrega en la URL
+	 * "categorias=categoria1,categoria2"
+	 * 
+	 * @param categorias es la lista que se recibe del query params con las
+	 *                   categorias que el api filtrará a la hora de devolver la
+	 *                   lista
+	 * @param pageable   objecto pageable que se utiliza para la paginación, se
+	 *                   utiliza con el query param "page=n", tambien para modificar
+	 *                   la cantidad de registros que mostraran por pagina con
+	 *                   "size=n"
+	 * @return json con una lista de objetos tipo producto
+	 */
+	public Page<Producto> listadoProductosFiltered(
+			@RequestParam(name = "categorias", required = true) List<String> categorias, Pageable pageable) {
+		;
+
+		Page<Producto> consultaProductos = productoServicio.listaProductoFiltered(categorias, pageable);
+
 		return consultaProductos;
 	}
-	
+
 	@GetMapping(path = "/productos/categories", produces = "application/json")
-	public List<String> listadoCategories(){
+	public List<String> listadoCategories() {
 		return productoServicio.listaCategory();
 	}
 }
--- proyectoFinal.CapaDatos/src/main/java/com/programacionIV/proyectoFinal/servicios/ProductoServicio.java
+++ proyectoFinal.CapaDatos/src/main/java/com/programacionIV/proyectoFinal/servicios/ProductoServicio.java
@@ -1,15 +1,9 @@
 package com.programacionIV.proyectoFinal.servicios;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 
 import com.programacionIV.proyectoFinal.entidades.Producto;
@@ -30,14 +24,21 @@ public class ProductoServicio {
 
 	}
 
+	/**
+	 * Devuelve una lista de productos filtrada por una lista de categorias brindada
+	 * 
+	 * @param categoria es la lista que se recibe con las categorias que se
+	 *                   filtrarán a la hora de devolver la lista
+	 * @param pageable   objecto pageable que se utiliza para la paginación
+	 * @return lista de objetos tipo producto
+	 */
 	public Page<Producto> listaProductoFiltered(List<String> categoria, Pageable pageable) {
-		
 
 		return productoRepository.findByCategoriaIn(categoria, pageable);
 
 	}
-	
-	public List<String> listaCategory(){
+
+	public List<String> listaCategory() {
 		return productoRepository.findDistinctCategory();
 	}
 
