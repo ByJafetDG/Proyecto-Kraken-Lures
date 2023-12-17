7912c916ea31038ac15d902bbde205d145e012bf
JafetDG <xjafetduartex@gmail.com>
2023-12-17 16:16:19 -0600

    Metodo POST en pedidos

--- proyectoFinal.CapaDatos/src/main/java/com/programacionIV/proyectoFinal/api/PedidoController.java
+++ proyectoFinal.CapaDatos/src/main/java/com/programacionIV/proyectoFinal/api/PedidoController.java
@@ -1,14 +1,19 @@
 package com.programacionIV.proyectoFinal.api;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.programacionIV.proyectoFinal.entidades.Pedido;
 import com.programacionIV.proyectoFinal.servicios.PedidoServicio;
@@ -45,5 +50,16 @@ public class PedidoController {
 
 		return consultaPedidos;
 	}
+	
+	@PostMapping("/pedidos/crear")
+    public ResponseEntity<Pedido> crearPedido(@RequestBody Pedido pedido) {
+        Pedido nuevoPedido = pedidoServicio.crearPedido(pedido);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(nuevoPedido.getPedidoId())
+                .toUri();
+        return ResponseEntity.created(location).body(nuevoPedido);
+    }
+	
 
 }
